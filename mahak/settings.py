99b73f7d0eb3b89b1ea52316b5b86f5ef3853f72
Hitesh Prajapati <hp@Hiteshs-MacBook-Air.local>
2026-05-10 13:32:14 +0530

    SQLITE DB

--- mahak/settings.py
+++ mahak/settings.py
@@ -79,17 +79,23 @@ WSGI_APPLICATION = 'mahak.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mahak',
-        'USER': 'root',
-        'PASSWORD': 'Admin@123',    #root
-        'HOST': 'localhost', 
-        'PORT': '3306',       
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'mahak',
+#         'USER': 'root',
+#         'PASSWORD': 'Admin@123',    #root
+#         'HOST': 'localhost', 
+#         'PORT': '3306',       
+#     }
+# }
 
 
 # Password validation
